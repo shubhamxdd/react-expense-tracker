68c61929d79635a223984b4442b6bf881f2d0c1f
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-07-25 17:21:36 +0530

    Before form

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,17 @@
 import { useState } from "react";
 import "./App.css";
 import Alert from "./components/Alert";
-import Button from "./components/Button";
-import ListGroup from "./components/ListGroup";
+// import Button from "./components/Button";
+// import ListGroup from "./components/ListGroup";
 import LikeButton from "./components/LikeButton";
-// import Baigan from "./components/Baigan";
 import Navbar from "./components/Navbar";
 import Cart from "./components/Cart";
 import Expandable from "./components/Expandable";
 
 function App() {
   const [showFull, setShowFull] = useState(false);
-  const [cart, setCart] = useState({
-    discount: 0.1,
-    items: [
-      {
-        id: 1,
-        title: "Pizza 1",
-        quantity: 1,
-      },
-      {
-        id: 2,
-        title: "Pizza 2",
-        quantity: 1,
-      },
-    ],
-  });
-  // const [pizza, setPizza] = useState({
-  //   name: "Pizza Name",
-  //   toppings: ["Cheese"],
-  // });
-
-  // const [game, setGame] = useState({
-  //   id: 1,
-  //   player: {
-  //     name: "Shubham",
-  //   },
-  // });
 
   const handleGameClick = () => {
-    setCart({
-      ...cart,
-      items: cart.items.map((item) =>
-        item.id === 1 ? { ...item, quantity: 2 } : item
-      ),
-    });
-    console.log(cart);
-
-    // setPizza({
-    //   ...pizza,
-    //   toppings: [...pizza.toppings, "Goo"],
-    // });
-    // console.log(pizza);
-    // setGame({
-    //   ...game,
-    //   player: {
-    //     ...game.player,
-    //     name: "Shubhammmmmmmmmmmmmmm",
-    //   },
-    // });
-    // console.log(game);
     console.log("clicked");
   };
 
@@ -76,7 +28,7 @@ function App() {
   const onClear = () => {
     setCartItems([]);
   };
-  const item = ["London", "Paris", "New York", "Tokyo", "Delhi", "Dubai"];
+  // const item = ["London", "Paris", "New York", "Tokyo", "Delhi", "Dubai"];
   const showFullFunc = () => {
     setShowFull(!showFull);
     console.log("clicked");
@@ -90,28 +42,28 @@ function App() {
       <button onClick={showFullFunc}>
         Show {showFull ? "Less" : "Full"} Text
       </button>
-      <ListGroup
+      {/* <ListGroup
         items={item}
         title="Cities"
         onSelectItem={(item: string) => {
           console.log(item);
         }}
-      />
+      /> */}
       <Alert btnState={btnState} onClick={onClickAlert}>
         Alert <span>OKAY!!</span>
       </Alert>
-      <Button
+      {/* <Button
         color="secondary"
         text="Button text"
         onClick={() => {
           setBtnState(true);
         }}
-      />
+      /> */}
       <LikeButton liked={liked} onClick={onLike} />
       {/* <Baigan /> */}
       <Navbar cartItemsCount={cartItems.length} />
       <Cart cartItem={cartItems} onClear={onClear} />
-      <button onClick={handleGameClick}>Click to change name </button>
+      <button onClick={handleGameClick}>Click to change </button>
     </>
   );
 }
