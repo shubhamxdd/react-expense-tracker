d7b0a42d9db5ba599141c541e2eb3b61cdfbce3d
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-07-26 15:39:01 +0530

    Validation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,16 @@
 import { useForm, FieldValues } from "react-hook-form";
+
+interface FormData {
+  name: string;
+  age: number;
+}
+
 const Form = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
   const onSubmit = (data: FieldValues) => {
     console.log(data);
   };
@@ -16,8 +26,14 @@ const Form = () => {
             type="text"
             id="name"
             className="form-control"
-            {...register("name")}
+            {...register("name", { required: true, minLength: 3 })}
           />
+          {errors.name?.type === "required" && (
+            <p className="text-danger">Name is required</p>
+          )}
+          {errors.name?.type === "minLength" && (
+            <p className="text-danger">Name must have alteast 3 alphabets</p>
+          )}
         </div>
         <div className="mb-3">
           <label htmlFor="age" className="form-label">
